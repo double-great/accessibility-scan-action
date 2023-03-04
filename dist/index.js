2d57381992536d666334242e27d11f7a53744b41
Katy DeCorah <katydecorah@gmail.com>
2023-03-03 20:45:05 -0500

    Draft

--- dist/index.js
+++ dist/index.js
@@ -1,48 +1,26 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const process_1 = require("process");
 const nodePath = process_1.argv[0];
-const npmPath = nodePath.replace(/node$/, "npm");
-console.log("Installing dependencies...");
-console.log(`nodePath: ${nodePath}`);
-console.log(`npmPath: ${npmPath}`);
-(0, child_process_1.execFileSync)(nodePath, [npmPath, "ci", "--omit=dev"], {
+// const npmPath = nodePath.replace(/node$/, "npm");
+const binPath = nodePath.replace(/node$/, "");
+console.log(`binPath: ${binPath}`);
+(0, child_process_1.execSync)(`cd ${binPath} && ls`, {
     stdio: "inherit",
     cwd: __dirname,
 });
-Promise.resolve().then(() => __importStar(require("./action.js"))).then(({ action }) => __awaiter(void 0, void 0, void 0, function* () {
-    yield action();
-}));
+/*
+console.log("Installing dependencies...");
+console.log(`nodePath: ${nodePath}`);
+console.log(`npmPath: ${npmPath}`);
+
+execFileSync(nodePath, [npmPath, "ci", "--omit=dev"], {
+  stdio: "inherit",
+  cwd: __dirname,
+});
+
+import("./action.js").then(async ({ action }) => {
+  await action();
+});
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { execFileSync } from "child_process";
+import { execSync } from "child_process";
 import { argv } from "process";
 
 const nodePath = argv[0];
-const npmPath = nodePath.replace(/node$/, "npm");
+// const npmPath = nodePath.replace(/node$/, "npm");
 
+const binPath = nodePath.replace(/node$/, "");
+console.log(`binPath: ${binPath}`);
+execSync(`cd ${binPath} && ls`, {
+  stdio: "inherit",
+  cwd: __dirname,
+});
+/*
 console.log("Installing dependencies...");
 console.log(`nodePath: ${nodePath}`);
 console.log(`npmPath: ${npmPath}`);
@@ -16,3 +23,4 @@ execFileSync(nodePath, [npmPath, "ci", "--omit=dev"], {
 import("./action.js").then(async ({ action }) => {
   await action();
 });
+*/
