43d127daf46ae5117582bbce80bc0c966ad137b7
Katy DeCorah <katydecorah@gmail.com>
2023-03-03 20:48:19 -0500

    Draft

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
 import { execFileSync } from "child_process";
 import { argv } from "process";
 
-const nodePath = argv[0];
-const npmPath = nodePath.replace(/node$/, "npm");
+const npmPath = argv[0].replace(/node$/, "npm");
 
 console.log("Installing dependencies...");
-console.log(`nodePath: ${nodePath}`);
-console.log(`npmPath: ${npmPath}`);
 
-execFileSync(npmPath, ["-v"], {
-  stdio: "inherit",
-  cwd: __dirname,
-});
-
-/*
-execFileSync(nodePath, [npmPath, "ci", "--omit=dev"], {
+execFileSync(npmPath, ["ci", "--omit=dev"], {
   stdio: "inherit",
   cwd: __dirname,
 });
@@ -22,5 +13,3 @@ execFileSync(nodePath, [npmPath, "ci", "--omit=dev"], {
 import("./action.js").then(async ({ action }) => {
   await action();
 });
-
-*/
