b3262628d23f3914a740f4ba0136bc90e1268784
Katy DeCorah <katydecorah@gmail.com>
2025-01-21 18:07:40 -0500

    fix axe version
    Fix
    
    draft
    
    draft
    
    draft

--- src/axe-info.ts
+++ src/axe-info.ts
@@ -1,11 +1,9 @@
-import Axe from "axe-core";
+import axe from "axe-core";
 import { injectable } from "inversify";
 
 @injectable()
 export class AxeInfo {
-  constructor(private readonly axe: typeof Axe = Axe) {}
-
-  public get version(): string {
-    return this.axe.version;
+  public get version() {
+    return axe.version;
   }
 }
